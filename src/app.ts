4efd10d66531d4b053053c004a565238998b821c
samuel <amagbakhensamuel@gmail.com>
2024-12-05 09:09:43 +0100

    Second commit

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
-import { v4 as uuidv4 } from "uuid";
 import {
     START,
     END,
@@ -9,17 +8,21 @@ import {
     MemorySaver,
 } from "@langchain/langgraph";
 import { ChatOpenAI } from '@langchain/openai';
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
 const app: Application = express();
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 
 const llm = new ChatOpenAI({
     apiKey: process.env.OPENAI_API_KEY,
     model: "gpt-4o-mini",
     temperature: 0.7
 });
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
 
 // Define the function that calls the model
 const callModel = async (state: typeof MessagesAnnotation.State) => {
@@ -54,18 +57,18 @@ app.post('/api/v1/chat', async (req, res) => {
     ];
     const config = { configurable: { thread_id: thread_id } };
     const response=  await chatbotApp.invoke({ messages: input }, config)
-      // .then((response) => {
-      //     console.log(response.messages[response.messages.length - 1]);
-      // })
 
-    res.json({
-        message: "success",
-        data: response.messages[response.messages.length - 1],
-        thread_id: thread_id,
+    res.status(200).json({
+        status: "success",
+        message: "Message sent successfully",
+        data: {
+            messages: response.messages[response.messages.length - 1],
+            thread_id: thread_id,
+        },
     });
 })
 
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
 })
