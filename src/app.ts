50cec1a1e7c710ea7a1c0f99991b104b36fbeb84
samuel <amagbakhensamuel@gmail.com>
2024-12-05 09:19:50 +0100

    Third commit - AI chatbot is moving

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,20 @@ import {
 } from "@langchain/langgraph";
 import { ChatOpenAI } from '@langchain/openai';
 import { v4 as uuidv4 } from "uuid";
+import {
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+} from "@langchain/core/prompts";
 
 dotenv.config();
 
 const app: Application = express();
 const PORT = 8000;
+
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
 
 const llm = new ChatOpenAI({
     apiKey: process.env.OPENAI_API_KEY,
@@ -21,13 +30,29 @@ const llm = new ChatOpenAI({
     temperature: 0.7
 });
 
-// Middleware to parse JSON request bodies
-app.use(express.json());
+const prompt = ChatPromptTemplate.fromMessages([
+    [
+        "system",
+        `
+        You are Zeus, an AI chatbot designed to answer only questions related to cryptocurrency and Roqqu. Roqqu is a platform for buying, selling, and managing cryptocurrency. You are polite, knowledgeable, and concise. If a user asks a question unrelated to cryptocurrency or Roqqu, politely decline to answer and remind them of your purpose.
+
+        For example:
+        - If asked about cryptocurrency, provide expert insights or factual information.
+        - If asked about Roqqu, explain its features and use cases.
+        - If asked about anything else (e.g., sports, weather, random topics), respond with: "I'm sorry, but I can only assist with questions about cryptocurrency and Roqqu."
+
+        Always address users respectfully and refer to yourself as Zeus.`,
+    ],
+    new MessagesPlaceholder("messages"),
+]);
+
 
 // Define the function that calls the model
 const callModel = async (state: typeof MessagesAnnotation.State) => {
-    const response = await llm.invoke(state.messages);
-    return { messages: response };
+    const chain = prompt.pipe(llm)
+    const response = await chain.invoke(state);
+    // Update message history with response:
+    return { messages: [response] };
 }
 
 const workflow = new StateGraph(MessagesAnnotation)
@@ -41,7 +66,6 @@ const memory = new MemorySaver();
 
 // Compile the workflow and create the app
 const chatbotApp = workflow.compile({ checkpointer: memory });
-
 
 app.get('/', (req, res) => {
     res.send('Hello World');
